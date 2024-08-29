6bca6925b36e16045f9c9fab44da69099e0c60f8
Arthur De Bellis <debellisarthur@gmail.com>
2024-08-29 20:59:23 -0300

    Lógica de uso de tasks para paralelismo

--- Auvo/Controllers/CalculoRh.cs
+++ Auvo/Controllers/CalculoRh.cs
@@ -1,6 +1,7 @@
 ﻿using Auvo.Interfaces;
 using Auvo.Models;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Globalization;
 using System.Text.RegularExpressions;
 
@@ -9,6 +10,10 @@ namespace Auvo.Controllers
     internal class CalculoRh : ICalculoRh
     {
         private readonly ILogger _logger;
+        private static readonly object lock1 = new object();
+        private static readonly object lock2 = new object();
+        private static readonly object lock3 = new object();
+        private static readonly object lock4 = new object();
 
         public CalculoRh(ILogger logger)
         {
@@ -35,6 +40,8 @@ namespace Auvo.Controllers
 
                 foreach (ArquivoCSV arquivo in arquivos)
                 {
+                    List<Task> listaDeTasks = new List<Task>();
+                    
                     string nomearquivo = arquivo.NomeArquivo.Substring(0, arquivo.NomeArquivo.LastIndexOf('.')); // Retira a extensão .csv da string
                     string[] infos = nomearquivo.Split('-');
 
@@ -55,92 +62,14 @@ namespace Auvo.Controllers
                     
                     foreach (var funcionario in infosPorFuncionario)
                     {
-
-                        var informacoesFuncionario = funcionario.ToList();
-
-                        string nome  = informacoesFuncionario[0].Nome;
-                        string codigo  = informacoesFuncionario[0].Codigo;
-                        double totalReceber  = 0;
-                        double horasExtras  = 0;
-                        double horasDebito  = 0;
-                        double diasFalta  = 0;
-                        double diasExtras  = 0;
-                        int diasTrabalhados  = 0;
-
-                        string expressaoRegular = @"[R$ ]";
-
-                        double valorHoraGlobal = Double.Parse(Regex.Replace(informacoesFuncionario[0].ValorHora, expressaoRegular, ""));
-
-                        int mes = DateTime.ParseExact(departamento.MesVigencia, "MMMM", new CultureInfo("pt-BR")).Month;
-                        int quantidadeDeDiasUteis = ObtemQuantidadeDeDiasUteis(departamento.AnoVigencia, mes);
-                        
-                        Funcionario func = new Funcionario
-                        {
-                            Nome = nome,
-                            Codigo = codigo,
-                        };
-
-                        foreach (var informacao in informacoesFuncionario)
-                        {
-                            DateTime data = ObtemInformacaoDateTime(informacao.Data, "dd/MM/yyyy");
-                            DateTime entrada = ObtemInformacaoDateTime(informacao.Entrada, "HH:mm:ss");
-                            DateTime saida = ObtemInformacaoDateTime(informacao.Saida, "HH:mm:ss");
-                            var horasTotaisDia = saida - entrada;
-                            string[] horariosAlmoco = informacao.Almoco.Split(" - ");
-                            DateTime almocoInicio = ObtemInformacaoDateTime(horariosAlmoco[0], "HH:mm");
-                            DateTime almocoFim = ObtemInformacaoDateTime(horariosAlmoco[1], "HH:mm");
-                            var horasTotaisAlmoco = almocoFim - almocoInicio;
-                            var totalDeHoras = horasTotaisDia - horasTotaisAlmoco;
-
-                            double valorHora = Double.Parse(Regex.Replace(informacao.ValorHora, expressaoRegular, ""));
-
-                            double horaExtraDia = totalDeHoras.TotalHours - 8;
-
-                            bool eFinalDeSemana = data.DayOfWeek == DayOfWeek.Saturday || data.DayOfWeek == DayOfWeek.Sunday;
-
-                            // Para finais de semana todas as horas são extras
-                            if (eFinalDeSemana)
-                            {
-                                diasExtras += 1;
-                                horasExtras += totalDeHoras.TotalHours;
-                                totalExtras += totalDeHoras.TotalHours * valorHora;
-                            }
-                            else
-                            {
-                                diasTrabalhados += 1;
-
-                                // Se houver horas extras as horas extras totais também são contabilizadas
-                                if(horaExtraDia >= 0)
-                                {
-                                    // Caso o valor da variavel horaExtraDia seja zero, não há alteração no valor original das variáveis
-                                    horasExtras += horaExtraDia;
-                                    totalExtras += horaExtraDia * valorHora;
-                                }
-                                else
-                                {
-                                    horasDebito += 8 - totalDeHoras.TotalHours;
-                                    totalDescontos += (8 - totalDeHoras.TotalHours) * valorHora;
-                                }
-
-                            }
-
-                            // O funcionário receberá o total de horas trabalhadas * valor de sua hora independente da quantidade de horas
-                            totalReceber += totalDeHoras.TotalHours * valorHora;
-
-                        }
-
-                        diasFalta = quantidadeDeDiasUteis - diasTrabalhados;
-                        totalDescontos = diasFalta * valorHoraGlobal;
-
-                        func.TotalReceber = Math.Round(totalReceber, 2);
-                        func.HorasExtras = Math.Round(horasExtras, 2);
-                        func.HorasDebito = Math.Round(horasDebito, 2);
-                        func.DiasFalta = diasFalta;
-                        func.DiasExtras = diasExtras;
-                        func.DiasTrabalhados = diasTrabalhados;
-
-                        funcionarios.Add(func);
+                        // Lógica de Paralelismo
+                            // A divisão do trabalho aqui é possível pela não necessidade de sincronicidade na leitura dos arquivos
+                                // e pela alta carga de operações nos cálculos de cada funcionário
+                        Task t = Task.Run(() => Rotina(funcionario, departamento, ref funcionarios, ref totalDescontos, ref totalExtras));
+                        listaDeTasks.Add(t);
                     }
+
+                    Task.WhenAll(listaDeTasks).Wait();
 
                     totalPagar = funcionarios.Sum(x => x.TotalReceber);
 
@@ -180,6 +109,105 @@ namespace Auvo.Controllers
         internal DateTime ObtemInformacaoDateTime(string data, string formato)
         {
             return DateTime.ParseExact(data, formato, new CultureInfo("pt-BR"));
+        }
+
+        internal void Rotina(
+            IGrouping<string, InformacoesFuncionario> funcionario,
+            InformacoesDepartamento departamento,
+            ref List<Funcionario> funcionarios, 
+            ref double totalDescontos,
+            ref double totalExtras
+        ){
+            var informacoesFuncionario = funcionario.ToList();
+
+            string nome = informacoesFuncionario[0].Nome;
+            string codigo = informacoesFuncionario[0].Codigo;
+            double totalReceber = 0;
+            double horasExtras = 0;
+            double horasDebito = 0;
+            double diasFalta = 0;
+            double diasExtras = 0;
+            int diasTrabalhados = 0;
+
+            string expressaoRegular = @"[R$ ]";
+
+            double valorHoraGlobal = Double.Parse(Regex.Replace(informacoesFuncionario[0].ValorHora, expressaoRegular, ""));
+
+            int mes = DateTime.ParseExact(departamento.MesVigencia, "MMMM", new CultureInfo("pt-BR")).Month;
+            int quantidadeDeDiasUteis = ObtemQuantidadeDeDiasUteis(departamento.AnoVigencia, mes);
+
+            Funcionario func = new Funcionario
+            {
+                Nome = nome,
+                Codigo = codigo,
+            };
+
+            foreach (var informacao in informacoesFuncionario)
+            {
+                DateTime data = ObtemInformacaoDateTime(informacao.Data, "dd/MM/yyyy");
+                DateTime entrada = ObtemInformacaoDateTime(informacao.Entrada, "HH:mm:ss");
+                DateTime saida = ObtemInformacaoDateTime(informacao.Saida, "HH:mm:ss");
+                var horasTotaisDia = saida - entrada;
+                string[] horariosAlmoco = informacao.Almoco.Split(" - ");
+                DateTime almocoInicio = ObtemInformacaoDateTime(horariosAlmoco[0], "HH:mm");
+                DateTime almocoFim = ObtemInformacaoDateTime(horariosAlmoco[1], "HH:mm");
+                var horasTotaisAlmoco = almocoFim - almocoInicio;
+                var totalDeHoras = horasTotaisDia - horasTotaisAlmoco;
+
+                double valorHora = Double.Parse(Regex.Replace(informacao.ValorHora, expressaoRegular, ""));
+
+                double horaExtraDia = totalDeHoras.TotalHours - 8;
+
+                bool eFinalDeSemana = data.DayOfWeek == DayOfWeek.Saturday || data.DayOfWeek == DayOfWeek.Sunday;
+
+                // Para finais de semana todas as horas são extras
+                if (eFinalDeSemana)
+                {
+                    diasExtras += 1;
+                    horasExtras += totalDeHoras.TotalHours;
+                    lock (lock1)
+                        totalExtras += totalDeHoras.TotalHours * valorHora; // Referência
+                }
+                else
+                {
+                    diasTrabalhados += 1;
+
+                    // Se houver horas extras as horas extras totais também são contabilizadas
+                    if (horaExtraDia >= 0)
+                    {
+                        // Caso o valor da variavel horaExtraDia seja zero, não há alteração no valor original das variáveis
+                        horasExtras += horaExtraDia;
+                        lock (lock2)
+                            totalExtras += horaExtraDia * valorHora; // Referência
+                    }
+                    else
+                    {
+                        horasDebito += 8 - totalDeHoras.TotalHours;
+                        lock (lock3)
+                            totalDescontos += (8 - totalDeHoras.TotalHours) * valorHora; // Referência
+                    }
+
+                }
+
+                // O funcionário receberá o total de horas trabalhadas * valor de sua hora independente da quantidade de horas
+                totalReceber += totalDeHoras.TotalHours * valorHora;
+
+            }
+
+            diasFalta = quantidadeDeDiasUteis - diasTrabalhados;
+
+            func.TotalReceber = Math.Round(totalReceber, 2);
+            func.HorasExtras = Math.Round(horasExtras, 2);
+            func.HorasDebito = Math.Round(horasDebito, 2);
+            func.DiasFalta = diasFalta;
+            func.DiasExtras = diasExtras;
+            func.DiasTrabalhados = diasTrabalhados;
+
+            lock (lock4)
+            {
+                totalDescontos += diasFalta * valorHoraGlobal; // Referência
+                funcionarios.Add(func); // Referência
+            }
         }
     }
 }
